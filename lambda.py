fc7acd81db60bb14e1424e1c43b4489642abd0ea
jeson2g <1120834382>
2018-09-08 17:47:23 +0800

    lambda

---
+++ lambda.py
@@ -0,0 +1,16 @@
+# a = 1 # 0001
+# b = 2 # 0010
+# print(a, b)
+# c = a ^ b # 0011
+# a = c ^ a #
+# b = c ^ a
+# # b = a ^ b
+# print(a, b)
+
+def multipliers():
+    return [lambda x, i=i: i*x for i in range(4)]
+
+print ([m(2) for m in multipliers()] )
+"""
+[6, 6, 6, 6]
+"""
