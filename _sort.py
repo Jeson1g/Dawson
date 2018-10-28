c0d8e0877e3ed6f386549fc0849d202b27229430
baijie <1120834382@qqcom>
2018-10-29 00:34:03 +0800

    格式化

--- _sort.py
+++ _sort.py
@@ -1,16 +1,16 @@
-def swop(li, i):
+def swap(li, i):
     """交换两个变量的值"""
     li[i], li[i + 1] = li[i + 1], li[i]
 
 
-def bubble_sort(a_list):
+def bubble_sort(li):
     """冒泡排序"""
-    n = len(a_list)
+    n = len(li)
     for i in range(n - 1):
         count = 0
         for j in range(n - 1 - i):
-            if a_list[j] > a_list[j + 1]:
-                swop(a_list, j)
+            if li[j] > li[j + 1]:
+                swap(li, j)
             count += 1
         if 0 == count:
             break
@@ -18,14 +18,14 @@ def bubble_sort(a_list):
     return a_list
 
 
-def bubble_sorts(a_list):
+def bubble_sorts(li):
     """冒泡排序2"""
-    n = len(a_list)
+    n = len(li)
     for i in range(n - 1, 0, -1):
         count = 0
         for j in range(i):
-            if a_list[j] > a_list[j + 1]:
-                swop(a_list, j)
+            if li[j] > li[j + 1]:
+                swap(li, j)
             count += 1
         if 0 == count:
             break
@@ -33,31 +33,31 @@ def bubble_sorts(a_list):
     return a_list
 
 
-def select_sort(a_list):
+def select_sort(li):
     """选择排序"""
-    n = len(a_list)
+    n = len(li)
     for j in range(n - 1):
         min_index = j
         for i in range(j + 1, n):
-            if a_list[i] < a_list[min_index]:
+            if li[i] < li[min_index]:
                 min_index = i
         if 0 != min_index:
-            a_list[j], a_list[min_index] = a_list[min_index], a_list[j]
+            li[j], li[min_index] = li[min_index], li[j]
 
     return a_list
 
 
-def insert_sort(a_list):
+def insert_sort(li):
     """插入排序"""
-    n = len(a_list)
+    n = len(li)
     for j in range(n):
         for i in range(j, 0, -1):
-            if a_list[i] < a_list[i - 1]:
-                a_list[i], a_list[i - 1] = a_list[i - 1], a_list[i]
+            if li[i] < li[i - 1]:
+                li[i], li[i - 1] = li[i - 1], li[i]
 
 
 if __name__ == '__main__':
     a_list = [4, 88, 1, 2, 0, 78]
     print(a_list)
-    bubble_sort(a_list)
+    select_sort(a_list)
     print(a_list)
